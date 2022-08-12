4e02adf41336fe84fafcba82e93e62a8b8ae054c
akumar23 <kumar.aryan@gmail.com>
2022-08-12 14:53:52 -0700

    displayed image and name from API on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,25 +13,21 @@ export default function Home () {
 
   [id1, id2] = ids;
 
+  var hero1Url = "test";
+  var hero2Url = "test2";
+
+  var hero1Name = " ";
+  var hero2Name = " ";
+
   if (id1 !== undefined && id2 !== undefined) {
     const firstHero = trpc.useQuery(["get-hero-by-id", {id: id1}]);
     const secondHero = trpc.useQuery(["get-hero-by-id", {id: id2}]); 
 
-    var data1 = firstHero.data;
-    var data2 = secondHero.data;
+    hero1Url = firstHero.data?.image.url;
+    hero2Url = secondHero.data?.image.url;
 
-    console.log(data1);
-    console.log(data2);
-
-    /*
-    if (data1 !== undefined && data2 !== undefined) {
-      var hero1 = JSON.parse(JSON.stringify(data1));
-      var hero2 = JSON.parse(data2);
-    
-      console.log(hero1.name);
-      console.log(hero2.image);
-    }
-    */
+    hero1Name = firstHero.data?.name;
+    hero2Name = secondHero.data?.name;
 
   }
 
@@ -39,14 +35,20 @@ export default function Home () {
     <>
 
       <div className="h-screen w-screen flex flex-col justify-center">
-        <div className="text-2xl text-center"> Which Hero do you Like More? </div>
+        <div className="text-2xl text-center"> Which Character do you Like More? </div>
         
         <div className="p-3" />
         
-        <div className="border rounded p-8 flex justify-between"> 
-          <div className="w-16 h-16 bg-red-200"> {id1} </div>
+        <div className="border rounded p-20 flex justify-between"> 
+          <div className="w-16 h-16 bg-red-200"> 
+            <img src={hero1Url} />
+            <div> {hero1Name} </div>
+          </div>
           <div className="p-8"> vs </div>
-          <div className= "w-16 h-16 bg-red-200"> {id2} </div>
+          <div className= "w-16 h-16 bg-red-200">
+            <img src={hero2Url} />
+            <div> {hero2Name} </div>
+          </div>
         </div>
       </div>
       
