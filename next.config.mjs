01638101a8ea1fd38444612654ee493a8091b97a
Aryan Kumar <aryan.kumar@nalej.com>
2025-12-09 17:43:46 -0800

    updated packages

--- next.config.mjs
+++ next.config.mjs
@@ -15,4 +15,17 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    // Allow images from the local API proxy route
+    // Note: localhost is automatically allowed for local development
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.superherodb.com',
+        pathname: '/pictures2/portraits/**',
+      },
+    ],
+    // For API route images, we use unoptimized mode since they're proxied
+    unoptimized: false,
+  },
 });
